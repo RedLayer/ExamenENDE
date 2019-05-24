53b9310a1cebd88ea9993a61730a39a43a5d4920
Pablo Fernandez Alonso <48572835+RedLayer@users.noreply.github.com>
2019-05-24 13:59:04 +0200

    Cambios de Conflicto
    Cambios en los conflictos del archivo

--- src/Casa.java
+++ src/Casa.java
@@ -5,11 +5,10 @@ public class Casa {
 	private int codigoPostal;
 	
 	/**
-	 * Un equipo de ladrones deja la casa libre de germenes
-	 * y roba hasta el cobre de las paredes
+	 * Un equipo de limpiadores deja la casa libre de germenes
+	 * y desinfecta todas las superficies
 	 */
 	public void limpiarLaCasa() {
-		String mensajito = "Te hemos limpiado la casa, esta arruinado";
-		System.out.println(mensajito);
+		System.out.println("Te hemos limpiado la casa, esta desinfectada");
 	}
 }
